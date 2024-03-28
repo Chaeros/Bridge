c684777e4ef1daa657cd0e732fc47bbf7250ebc8
ensk26 <tjensk26@gmail.com>
2024-03-28 17:21:50 +0900

    Feat : 게시판 페이징 구현 및 네비게이션 바 구현

--- src/main/java/com/ssafy/bridge/freeboard/service/FreeBoardService.java
+++ src/main/java/com/ssafy/bridge/freeboard/service/FreeBoardService.java
@@ -2,23 +2,28 @@ package com.ssafy.bridge.freeboard.service;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import com.ssafy.bridge.freeboard.dto.request.FreeBoardAddRequest;
 import com.ssafy.bridge.freeboard.dto.request.FreeBoardModifyRequest;
+import com.ssafy.bridge.freeboard.dto.request.displayFreeBoardListRequest;
 import com.ssafy.bridge.freeboard.dto.response.FreeBoardResponse;
+import com.ssafy.bridge.util.PageNavigation;
 
 public interface FreeBoardService {
 	/*
-	 * add : 추가
-	 * modify : 수정
-	 * remove : 삭제
-	 * search : 검색
-	 * display : 출력
+	 * add : 추가 modify : 수정 remove : 삭제 search : 검색 display : 출력
 	 */
-	
+
 	public int addFreeBoard(FreeBoardAddRequest board) throws SQLException;
+
 	public int modifyFreeBoard(FreeBoardModifyRequest board) throws SQLException;
+
 	public int removeFreeBoard(int no) throws SQLException;
+
 	public FreeBoardResponse searchByNoFreeBoard(int no) throws SQLException;
-	public List<FreeBoardResponse> displayFreeBoardList() throws SQLException;
+
+	public List<FreeBoardResponse> displayFreeBoardList(Map<String, Object> map) throws SQLException;
+
+	public PageNavigation makePageNavigation(Map<String, Object> map) throws Exception;
 }
--- src/main/webapp/freeboard/list.jsp
+++ src/main/webapp/freeboard/list.jsp
@@ -65,16 +65,7 @@ nav a {
 				</tbody>
 			</table>
 		</section>
-			<div class="pagination">
-				<a href="#">&laquo;</a> 
-				<a href="#">1</a> 
-				<a href="#">2</a> 
-				<a href="#">3</a> 
-				<a href="#">4</a> 
-				<a href="#">5</a> 
-				<a href="#">6</a>
-				<a href="#">&raquo;</a>
-			</div>
+		${navigation.navigator}
 	</main>
 	<jsp:include page="/footer.jsp" />
 </body>
